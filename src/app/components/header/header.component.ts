6171e1c097dbb7bf4ef5e61816e715ee5479b0e2
peggy <tawah.peggy@go-groups.net>
2021-07-01 19:19:25 -0400

    made modifications to the header

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -8,22 +8,22 @@ import { TranslateService } from '@ngx-translate/core';
 })
 export class HeaderComponent implements OnInit {
   browserLang : any;
-  constructor(  
-    public translate: TranslateService) {  
+  constructor(
+    public translate: TranslateService) {
     translate.addLangs(['English', 'French']);
 
-    if (localStorage.getItem('locale')) {  
-      this. browserLang = localStorage.getItem('locale');  
-      translate.use(this.browserLang.match(/English|French/) ? this.browserLang : 'English');  
-    } else {  
-      localStorage.setItem('locale', 'English');  
-      translate.setDefaultLang('English');  
-    }  
-  }  
-  changeLang(language: string) {  
-    localStorage.setItem('locale', language);  
-    this.translate.use(language);  
-  }  
+    if (localStorage.getItem('locale')) {
+      this. browserLang = localStorage.getItem('locale');
+      translate.use(this.browserLang.match(/English|French/) ? this.browserLang : 'English');
+    } else {
+      localStorage.setItem('locale', 'English');
+      translate.setDefaultLang('English');
+    }
+  }
+  changeLang(language: string) {
+    localStorage.setItem('locale', language);
+    this.translate.use(language);
+  }
 
   ngOnInit(): void {
   }
